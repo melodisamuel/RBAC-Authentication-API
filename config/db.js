351e2a96e772f7c83b77c040adc06ab87fc44f00
Melody <melodysamuelcodes@gmail.com>
2025-02-05 00:13:42 +0100

    # Modified db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,38 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv')
 
-const connectDb = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('MongoDB Connected');
-    } catch(err) {
-        console.log(err.message);
-        process.exit(1);
-    }
-}
+process.on("uncaughtException", (err) => {
+    console.log(e);
+    console.log(err.name, err.message);
+    process.exit(1);
+  });
 
-module.exports = connectDb;
+const app = require('./app');
+
+// Loadind environmental variables
+dotenv.config({ path: './config.env' })
+
+
+// Connect to mongoDB
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+
+mongoose.connect(DB, {}).then(() => console.log('DB connection succesful'));
+
+
+
+// PORT
+const port = process.env.PORT || 3000;
+
+// SERVER
+const server = app.listen(port, () => {
+    console.log(`App running on ${port}`);
+})
+
+process.on("unhandledRejection", (err) => {
+    console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+    console.log(err.name, err.message);r
+    server.close(() => {
+      process.exit(1);
+    });
+  });
+
