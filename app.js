deacf242c468a019964bf39a75683dbd804e18b8
Melody <melodysamuelcodes@gmail.com>
2025-02-05 00:22:50 +0100

    # Modidied env file

--- app.js
+++ app.js
@@ -11,8 +11,15 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+})
 
 // Handling unhandled routes 
 app.all('*', (req, res, next) => {
     next(new AppError(`Can't find ${req.originalUrl} on this server!`))
 });
+
+
+module.exports = app;
